f31855d9b09b98992349acd1f1be56316c4d7e00
James White <whitejaad@gmail.com>
2023-07-10 20:44:50 -0700

    added to test

--- spec/reverse_odd_words_spec.rb
+++ spec/reverse_odd_words_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'reverse_odd_words' do
     it 'reverses words with an odd number of letters' do
      expect(reverse_odd_words('Shmee is Captain Hooks best friend')).to eq('eemhS is niatpaC skooH best friend')
      expect(reverse_odd_words('me me me me')).to eq('me me me me')
+     expect(reverse_odd_words('dog me dog me')).to eq('god me god me')
 
     end
 end
