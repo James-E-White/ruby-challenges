41b12d4212d61c41cb3899383f3cdd2073876b7d
James White <whitejaad@gmail.com>
2023-07-14 15:00:35 -0700

    find missing numbers

---
+++ lib/find_missing_number.rb
@@ -0,0 +1,9 @@
+def find_missing_number(numbers)
+  complete_array = (1..20).to_a
+  missing_number = complete_array - numbers
+end
+
+
+numbers = [2, 1, 5, 4, 6, 9, 7, 8, 10, 11, 13, 15, 18, 19, 20]
+missing_number = find_missing_number(numbers)
+puts "The missing numbers are: #{missing_number}"
