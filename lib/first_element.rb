f4511b2b6c8ac84477679d693e714e703d69f849
James White <whitejaad@gmail.com>
2023-07-14 11:41:03 -0700

    easy challenges warm up

---
+++ lib/first_element.rb
@@ -0,0 +1,13 @@
+def first_element(array)
+    first_element = array.first
+end
+
+p first_element(["love", "earth", "fire", "water"])
+
+
+
+# def first_element(array)
+#     array.first
+# end
+
+# p first_element(["love", "earth", "fire", "water"])
--- spec/first_element_spec.rb
+++ spec/first_element_spec.rb
@@ -1,1 +1,10 @@
-g
+require './lib/first_element'
+
+RSpec.describe 'first_element(array)' do 
+   it 'returns the first element of an array' do 
+    
+    expect(first_element(["love", "earth", "fire", "water"])).to eq('love')
+   end  
+end
+
+
