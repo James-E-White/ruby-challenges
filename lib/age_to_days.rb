b3dc5313fc4c74af9f5fab2d6ff2c835729d5d55
James White <whitejaad@gmail.com>
2023-07-16 14:16:58 -0700

    quick warmups

---
+++ lib/age_to_days.rb
@@ -0,0 +1,15 @@
+def age_in_days(num)
+    days_age = num * 365
+end
+
+p age_in_days(10)
+p age_in_days(100)
+
+#not going to test this one
+#conversely 
+
+def days_to_years(num)
+    age = num / 365
+end
+
+p days_to_years(3650)
