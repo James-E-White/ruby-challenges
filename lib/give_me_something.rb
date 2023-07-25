7ef21a0d545fdc59d1ea3528fd7da02c17cfd7e4
James White <whitejaad@gmail.com>
2023-07-25 11:22:43 -0700

    easy again need more warm up

---
+++ lib/give_me_something.rb
@@ -0,0 +1,7 @@
+def give_me_something(a)
+    give_me_something =  "something " + a
+end
+
+p give_me_something("is better than nothing")
+p give_me_something("Bob Jane")
+p give_me_something("something")
