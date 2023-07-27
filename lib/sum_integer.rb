313e9b2799e3e4a14a7bf690e02436f306b692a1
James White <whitejaad@gmail.com>
2023-07-27 09:58:33 -0700

    added BigDecimal notation not needed

--- lib/sum_integer.rb
+++ lib/sum_integer.rb
@@ -4,3 +4,5 @@ end
 
 p sum_integer(0.3, 0.7)
 p sum_integer(0.44, 0.88)
+p sum_integer(0.6666666, 0.99999999)
+#require 'bigdecimal' could have used this but ruby has built in method for floating point addition.
