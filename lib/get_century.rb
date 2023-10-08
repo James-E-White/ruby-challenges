5bb3a1e23e018ce3281542357c6b9e4012a6a0a4
James White <whitejaad@gmail.com>
2023-10-08 15:39:22 -0700

    update century

---
+++ lib/get_century.rb
@@ -0,0 +1,15 @@
+def get_century(year)
+  century = (year - 1) / 100 + 1
+
+  century_suffix = case century % 100
+                   when 11, 12, 13 then 'th'
+                   when 1 then 'st'
+                   when 2 then 'nd'
+                   when 3 then 'rd'
+                   else 'th'
+                   end
+
+  "#{century}#{century_suffix} century"
+end
+
+p get_century(2005)
