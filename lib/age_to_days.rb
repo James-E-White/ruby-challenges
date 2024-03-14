7f2277fe489a709198ca9b2b1134ebf22d43e438
James White <whitejaad@gmail.com>
2024-03-13 17:29:14 -0700

    babybasics

--- lib/age_to_days.rb
+++ lib/age_to_days.rb
@@ -1,12 +1,15 @@
 def age_in_days(num)
-    days_age = num * 365
+    if num < 0 
+        puts "Please enter a valid age"
+    else
+        days_age = num * 365
+    end
 end
 
 p age_in_days(10)
 p age_in_days(100)
 
-#not going to test this one
-#conversely 
+
 
 def days_to_years(num)
     age = num / 365
