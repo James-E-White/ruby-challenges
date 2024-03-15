a349dfdd72f4c1e9a595fd0602be272b155de178
James White <whitejaad@gmail.com>
2024-03-14 17:45:34 -0700

    animallegs

---
+++ lib/animal_legs.rb
@@ -0,0 +1,8 @@
+def animal_legs(chickens, cows, pigs)
+    total_legs = (chickens * 2) + (cows * 4) + (pigs * 4)
+     if total_legs > 0
+    return total_legs
+    else 
+     return "Buy some animals!"
+    end
+end
