e15f05498ed90b296c1c9307178ed031e1b1eb17
James White <whitejaad@gmail.com>
2025-01-13 16:03:11 -0800

    updates to add_odd_tests

--- spec/add_odd_to_n_spec.rb
+++ spec/add_odd_to_n_spec.rb
@@ -1,12 +1,12 @@
 require './lib/add_odd_to_n'
 
-RSpec.describe 'add_odd_to_n and add_even_to_n' do 
-    it 'adds all the odd numbers from 1 to n' do 
-        expect(add_odd_to_n(13)).to eq(49)
+RSpec.describe 'add_odd_to_n and add_even_to_n' do
+  it 'adds all the odd numbers from 1 to n' do
+    expect(add_odd_to_n(13)).to eq(49)
 
-    end
+  end
 
-    it 'adds all the even numbers from 2 to n' do 
-        expect(add_even_to_n(10)).to eq(30)
-    end
+  it 'adds all the even numbers from 2 to n' do 
+    expect(add_even_to_n(10)).to eq(30)
+  end
 end
