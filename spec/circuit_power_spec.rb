033394641607f985f867d4c2b449edaeedebbc0c
James White <whitejaad@gmail.com>
2025-01-16 16:37:03 -0800

    updated circuit_ppower

--- spec/circuit_power_spec.rb
+++ spec/circuit_power_spec.rb
@@ -1,7 +1,11 @@
-require './lib/circuit_power'
+require_relative '../lib/circuit_power'
 
 RSpec.describe 'circuit_power' do
   it 'returns the power of the circuit' do
-    expect(circuit_power(230, 10)).to eq(2300)
+    expect(circuit_power(230, 10)).to eq([2300, 'You dead!'])
+  end
+  
+  it 'returns "Did it tickle?" if the power is less than or equal to 1000' do
+    expect(circuit_power(23, 10)).to eq([230,'Did it tickle?'])
   end
 end
