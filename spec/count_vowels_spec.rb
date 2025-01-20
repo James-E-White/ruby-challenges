c6e3fd2043a54de31bf267a9d45ab93b6bf47606
James White <whitejaad@gmail.com>
2025-01-20 15:55:57 -0800

    updated counts vowels

--- spec/count_vowels_spec.rb
+++ spec/count_vowels_spec.rb
@@ -1,7 +1,16 @@
-require './lib/count_vowels'
+require_relative '../lib/count_vowels'
 
-RSpec.describe 'count_vowels'  do 
-    it 'counts the vowels of a string' do 
-      expect(count_vowels("Hello World!")).to eq(3)
-    end
+RSpec.describe 'count_vowels' do
+  it 'counts the vowels of a string' do
+    expect(count_vowels('Hello World!')).to eq(3)
+  end
+  it 'returns no vowels' do
+    expect(count_vowels('Hll Wrld')).to eq("No vowels")
+  end
+  it 'counts the consonants of a string' do
+    expect(count_consonants('Hello World!')).to eq(7)
+  end
+  it 'returns no consonants' do
+    expect(count_consonants('aeiou')).to eq("No consonants")
+  end
 end
