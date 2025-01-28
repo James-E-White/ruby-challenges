8d657969a464c71f714437e742ca3ca35fe7f2b3
James White <whitejaad@gmail.com>
2025-01-27 19:34:56 -0800

    silly potatoes

--- spec/potatoes_spec.rb
+++ spec/potatoes_spec.rb
@@ -1,11 +1,14 @@
-require './lib/potatoes'
+require_relative '../lib/potatoes'
 
 RSpec.describe 'potatoes' do 
-    it 'counts the potatoes' do 
-        expect(potatoes("potato")).to eq(1)
-    end
+  it 'counts the potatoes' do 
+    expect(potatoes('potato')).to eq(1)
+  end
 
-    it 'counts only the potatoes' do
-        expect(potatoes("potato, tomato, potato")).to eq(2)
-    end
+  it 'counts only the potatoes' do
+    expect(potatoes('potato, tomato, potato')).to eq(2)
+  end
+  it 'returns you have 0 potatoes if there are no potatoes' do
+    expect(potatoes('tomato')).to eq('you have 0 potatoes')
+  end
 end
