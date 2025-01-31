5f75192a9170e9ab63d30cf8d937baaba41f617d
James White <whitejaad@gmail.com>
2025-01-31 11:52:56 -0800

    finished testing

--- spec/validate_pin_spec.rb
+++ spec/validate_pin_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe 'Validate_pin' do
         expect(validate_pin(1324)).to eq(true)
         expect(validate_pin(13245)).to eq(false)
     end
+    it 'tests if a pin is valide or not with 6 digits' do
+        expect(validate_pin(132456)).to eq(true)
+    end
 end
